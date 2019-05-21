d65623981ea894168f12a89b2e53c49555f7e47d
Roy Kim <rykim79@gmail.com>
2019-05-21 15:31:23 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Landing from './components/Landing'
 import './App.css';
+
+import Landing from './components/Landing'
 
 function App() {
   return (
     <div>
+      <header>
+        <span className="header">Barkster</span>
+      </header>
       <BrowserRouter>
         <Route exact path="/" component={Landing} />
       </BrowserRouter>
